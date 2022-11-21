12ec0bf651fe9473be459991870283821d53715e
maxiang <maxiang@haoshuo.com>
2022-11-21 18:32:08 +0800

    feat: add request Authorization

--- services/request.tsx
+++ services/request.tsx
@@ -1,12 +1,17 @@
 import "server-only";
-import { headers } from 'next/headers';
+import { headers } from "next/headers";
 
 const request = async ({ url, ...arg }: { url: string }) => {
-  const headersInstance = headers()
-  const authorization = headersInstance.get('authorization')
-  console.log('authorization',authorization)
-  let resp = await fetch(url, {
+  const headersInstance = headers();
+  const host = headersInstance.get("host");
+  const authorization = headersInstance.get("authorization");
+  let resp = await fetch("http://" + host + url, {
     ...arg,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authorization,
+    },
   });
   return resp.json();
 };
