7ce1f87ddbe579b7b6bef2409552db349c85adeb
tourssliver <theobaldnartha@gmail.com>
2022-11-22 22:54:19 +0800

    feat: add cursor

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,7 @@ import vel from 'public/avatar.jpeg'
 
 const Avatar = () => {
   return (
-    <div className="relative h-12 w-12 rounded-full">
+    <div className="relative h-12 w-12 rounded-full cursor-pointer">
       <Image src={vel} alt="avatar" width={48} height={48} className='rounded-full'></Image>
     </div>
   )
