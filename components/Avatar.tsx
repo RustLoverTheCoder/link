f1174b793ffd7f18d30bdd0ebea68135a3bcb8de
tourssliver <theobaldnartha@gmail.com>
2022-11-26 22:56:18 +0800

    feat: add page

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { memo } from 'react'
 import Image from 'next/image'
 import vel from 'public/avatar.jpeg'
 
 const Avatar = () => {
   return (
-    <div className="relative h-12 w-12 cursor-pointer rounded-full">
-      <Image src={'https://avatar.vercel.sh/rauchg'} alt="avatar" width={48} height={48} className="rounded-full"></Image>
+    <div className="group relative h-12 w-12 cursor-pointer">
+      <Image src={'https://avatar.vercel.sh/rauchg'} alt="avatar" width={48} height={48} className="rounded-full group-hover:rounded-xl"></Image>
     </div>
   )
 }
 
-export default Avatar
+export default memo(Avatar)
