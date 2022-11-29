59505b8c4fc490cb211aaca85612f350cf7f1e6b
tourssliver <theobaldnartha@gmail.com>
2022-11-29 22:11:57 +0800

    fix: web build

--- app/web/layout.tsx
+++ app/web/layout.tsx
@@ -1,14 +1,11 @@
 import { Suspense, use } from 'react'
 import Loading from './loading'
 import { List } from 'components'
-import { notFound } from 'next/navigation'
-import { login } from '../../services/auth'
-import { getCurrentUser } from 'utils/session'
+// import { notFound } from 'next/navigation'
+// import { getCurrentUser } from 'utils/session'
 import './page.css'
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const res = await login()
-  console.log('res', res)
   // const user = await getCurrentUser()
   // if (!user) {
   //   return notFound()
