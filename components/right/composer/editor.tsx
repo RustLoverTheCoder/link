a0d02c24827ae5a888bc1f94de88fc450f31a690
tourssliver <theobaldnartha@gmail.com>
2022-12-03 22:15:28 +0800

    feat: add Popover

--- components/right/composer/editor.tsx
+++ components/right/composer/editor.tsx
@@ -2,15 +2,22 @@
 import React, { memo } from 'react'
 import { EditorContent, useEditor, Editor as EditorType } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+// @ts-ignore
 import BulletList from '@tiptap/extension-bullet-list'
+import Placeholder from '@tiptap/extension-placeholder'
 
-const Editor = () => {
+const Editor = ({ onSend }: { onSend?: () => {} }) => {
+  const CustomPlaceholder = Placeholder.configure({
+    emptyEditorClass: 'is-editor-empty',
+    placeholder: '消息@tourssliver',
+  })
   const CustomBulletList = BulletList.extend({
     addKeyboardShortcuts() {
       return {
         Enter: ({ editor }: { editor: EditorType }) => {
           /// 发送
           /// 删除内容
+          !!onSend && onSend()
           editor.commands.clearContent(true)
           return true
         },
@@ -18,8 +25,8 @@ const Editor = () => {
     },
   })
   const editor = useEditor({
-    extensions: [StarterKit, CustomBulletList],
-    content: '<p>Hello World!</p>',
+    extensions: [StarterKit, CustomBulletList, CustomPlaceholder],
+    autofocus: true,
   })
 
   return <EditorContent editor={editor} />
--- components/right/composer/plus.tsx
+++ components/right/composer/plus.tsx
@@ -1,8 +1,36 @@
-import React, { memo } from 'react'
+'use client'
+
+import React, { memo, Fragment } from 'react'
 import { BsPlusCircleFill } from 'react-icons/bs'
+import { Transition, Popover } from '@headlessui/react'
 
 const Plus = () => {
-  return <BsPlusCircleFill className="h-6 w-6 cursor-pointer text-[#b9bbbe] hover:text-white" />
+  return (
+    <>
+      <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button>
+              <BsPlusCircleFill className="h-6 w-6 cursor-pointer text-[rgb(185,187,190)] hover:text-white" />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute bottom-[42px] -left-4 z-10">
+                <div className="h-[78px] w-[216px] rounded-lg bg-[rgb(64,68,75)]">213</div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </>
+  )
 }
 
 export default memo(Plus)
